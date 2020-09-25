62e8520389cf6d66ed6bced6f04dee8658c67c22
skz227 <60292210+skz227@users.noreply.github.com>
2020-09-25 14:34:07 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,23 +6,21 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    #mailserver= '127.0.0.1'
-    #port=1025
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket= socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver,port))
-    #recv = clientSocket.recv(1024).decode()
+    recv = clientSocket.recv(1024).decode()
     #print(recv)
-    #if recv[:3] != '220':
-	    #print('220 reply not received from server.')
+    if recv[:3] != '220':
+	    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand='HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    #recv1 = clientSocket.recv(1024).decode()
+    recv1 = clientSocket.recv(1024).decode()
     #print(recv1)
-    #if recv1[:3] != '250':
-	    #print('250 reply not received from server.')
+    if recv1[:3] != '250':
+	    print('250 reply not received from server.')
     
 
     # Send MAIL FROM command and print server response.
@@ -49,11 +47,9 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #if recv4[:3] != '354':
         #print('354 reply not received from server.')
     # Send message data.
-    My_message="I Love Cakes.\r\n"
-    clientSocket.send(My_message.encode())
+    clientSocket.send("I Love Cakes.\r\n".encode())
     # Message ends with a single period.
-    End_message=".\r\n"
-    clientSocket.send(End_message.encode())
+    clientSocket.send(".\r\n".encode())
     #recv5 = clientSocket.recv(1024).decode()
     #print(recv5)
     #if recv5[:3] != '250':
